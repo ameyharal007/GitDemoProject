a2026ac25c624b8ff921aef5121894085954e7f2
AMEY <AMEY@DESKTOP-0LAKPDR>
2022-03-26 09:41:37 +0530

    First commit by amey

---
+++ src/AmeyGitDemo/PrimeNo.java
@@ -0,0 +1,27 @@
+package AmeyGitDemo;
+
+public class PrimeNo {
+
+	public static void main(String[] args) {
+
+		int num=5;
+		int temp=0;
+		for(int i=2;i<(num-1);i++) {
+
+			if(num%i==0) {
+				temp=temp+1;
+			}
+
+		}
+		if(temp>0) {
+			System.out.println(num+" is not a prime number");
+		}
+		else {
+			System.out.println(num+" is a prime number");
+		}
+
+	}
+
+}
+
+
